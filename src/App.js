d21d6f7e759a892697b128502212ae881e19b95a
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-21 15:15:21 +0100

    Add: AuthContext

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import './App.css';
-import Plants from './components/Plants';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Account from './components/Account';
-import Navbar from './components/Navbar';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import Plants from "./components/Plants";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Account from "./components/Account";
+import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
-
+import { AuthContextProvider } from "./utils/authContext";
 
 function App() {
   const [authUser, setAuthUser] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState('false');
 
   const listen = onAuthStateChanged(auth, (user) => {
     if (user) {
       setAuthUser(user);
-      setIsLoggedIn(true)
     } else {
       setAuthUser(null);
-      setIsLoggedIn(false)
     }
   });
 
   useEffect(() => {
-
     return () => {
       listen();
     };
   }, [authUser]);
 
   return (
-    <div className="App">
-      <Router>
-      <Navbar />
+    <AuthContextProvider>
+      <div className="App">
+        <Router>
+          <Navbar />
           <Routes>
             <Route path="/" element={<Plants />} />
-            {isLoggedIn && <Route path="/account" element={<Account isLoggedIn={authUser} />} />}
+            <Route
+              path="/account"
+              element={<Account isLoggedIn={authUser} />}
+            />
           </Routes>
         </Router>
-    </div>
+      </div>
+    </AuthContextProvider>
   );
 }
 
