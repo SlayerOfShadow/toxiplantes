e94d8cede336a5f61babd787975fad9dbaab59c1
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-24 19:19:53 +0100

    Add: authState check to add plant

--- src/components/Plants.jsx
+++ src/components/Plants.jsx
@@ -1,99 +1,105 @@
-import { useEffect, useState } from 'react';
-import { getFirestore, collection, getDocs, getDoc } from 'firebase/firestore'; // Import the necessary Firebase Firestore functions
-import { getUrl } from '../firebase';
-import { Link, Route } from 'react-router-dom';
+import { useContext, useEffect, useState } from "react";
+import { getFirestore, collection, getDocs, getDoc } from "firebase/firestore"; // Import the necessary Firebase Firestore functions
+import { getUrl } from "../firebase";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../utils/authContext";
 
 const Plants = ({ firebaseApp }) => {
-    const [plants, setPlants] = useState([]);
-    const [plantUrls, setPlantUrls] = useState([]);
-    const [animalNames, setAnimalNames] = useState([[]]);
+  const [plants, setPlants] = useState([]);
+  const [plantUrls, setPlantUrls] = useState([]);
+  const [animalNames, setAnimalNames] = useState([[]]);
 
-    const fetchData = async () => {
-        const db = getFirestore(firebaseApp); // Get Firestore instance from firebaseApp
-        const plantsCollection = collection(db, 'plants'); // Reference to the "Plants" collection
-        const querySnapshot = await getDocs(plantsCollection); // Fetch the documents in the collection
+  const { authState, setAuthState } = useContext(AuthContext);
 
-        // Convert the querySnapshot to an array of plant objects
-        const plantsData = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
+  const fetchData = async () => {
+    const db = getFirestore(firebaseApp); // Get Firestore instance from firebaseApp
+    const plantsCollection = collection(db, "plants"); // Reference to the "Plants" collection
+    const querySnapshot = await getDocs(plantsCollection); // Fetch the documents in the collection
 
-        setPlants(plantsData); // Update the state with the fetched plants
-    };
+    // Convert the querySnapshot to an array of plant objects
+    const plantsData = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
 
-    const getAnimal = async (ref) => {
-        try {
-            const snapshot = await getDoc(ref);
-            if (snapshot.exists) {
-                const animalData = snapshot.data();
-                const animalName = animalData.name;
-                return animalName;
-            } else {
-                console.log("Document does not exist");
-                return null;
-            }
-        } catch (error) {
-            console.error("Error getting document:", error);
-            return null;
-        }
+    setPlants(plantsData); // Update the state with the fetched plants
+  };
+
+  const getAnimal = async (ref) => {
+    try {
+      const snapshot = await getDoc(ref);
+      if (snapshot.exists) {
+        const animalData = snapshot.data();
+        const animalName = animalData.name;
+        return animalName;
+      } else {
+        console.log("Document does not exist");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error getting document:", error);
+      return null;
     }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, [firebaseApp]);
+  useEffect(() => {
+    fetchData();
+  }, [firebaseApp]);
 
-    const getImgUrls = async () => {
-        const imgUrls = [];
-        for (const plant of plants) {
-            const url = await getUrl(plant.imageUrl);
-            imgUrls.push(url);
-        }
-        setPlantUrls(imgUrls);
-    };
+  const getImgUrls = async () => {
+    const imgUrls = [];
+    for (const plant of plants) {
+      const url = await getUrl(plant.imageUrl);
+      imgUrls.push(url);
+    }
+    setPlantUrls(imgUrls);
+  };
 
-    const fetchAnimalNames = async () => {
-        const names = await Promise.all(
-            plants.map(async (plant) => {
-                const animalNamesForPlant = await Promise.all(
-                    plant.animals.map(async (animalRef) => await getAnimal(animalRef))
-                );
-                return animalNamesForPlant;
-            })
+  const fetchAnimalNames = async () => {
+    const names = await Promise.all(
+      plants.map(async (plant) => {
+        const animalNamesForPlant = await Promise.all(
+          plant.animals.map(async (animalRef) => await getAnimal(animalRef))
         );
-        setAnimalNames(names);
-    };
-
-    useEffect(() => {
-        getImgUrls();
-        fetchAnimalNames();
-    }, [plants]);
-    
-    return (
-        <div className="plants">
-            <h2>My Plants:</h2>
-            <div className="all-plants">
-                {plants.map((plant, index) => (
-                    <div className="plant-name" key={plant.name}>
-                        {plant.name}
-                        <div className="plant-animals">
-                            {animalNames[index] && (
-                                <ul>
-                                    {animalNames[index].map((animal, animalIndex) => (
-                                        <li key={animalIndex}>{animal}</li>
-                                    ))}
-                                </ul>
-                            )}
-                        </div>
-                        <img className="plant-image" src={plantUrls[index]} alt='' />
-                    </div>
-                ))}
-                <Link to="/add-plant">
-                    <button>ADD A PLANT</button>
-                </Link>
-            </div>
-        </div>
+        return animalNamesForPlant;
+      })
     );
+    setAnimalNames(names);
+  };
+
+  useEffect(() => {
+    getImgUrls();
+    fetchAnimalNames();
+  }, [plants]);
+
+  return (
+    <div className="plants">
+      <h2>My Plants:</h2>
+      <div className="all-plants">
+        {plants.map((plant, index) => (
+          <div className="plant-name" key={plant.name}>
+            {plant.name}
+            <div className="plant-animals">
+              {animalNames[index] && (
+                <ul>
+                  {animalNames[index].map((animal, animalIndex) => (
+                    <li key={animalIndex}>{animal}</li>
+                  ))}
+                </ul>
+              )}
+            </div>
+            <img className="plant-image" src={plantUrls[index]} alt="" />
+          </div>
+        ))}
+
+        {authState && (
+          <Link to="/add-plant">
+            <button>ADD A PLANT</button>
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Plants;
