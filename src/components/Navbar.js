db819cf4eadc0f3b7d3679167d9a50ad9d2fd50a
Greg0s <gregoire.tinnes@outlook.fr>
2023-11-24 19:12:04 +0100

    Add: auth related display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,42 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import AuthDetails from './AuthDetails'
+import { AuthContext } from "../utils/authContext";
+import { useContext } from "react";
+import { getAuth, signOut } from "firebase/auth";
 
 const Navbar = () => {
-    return ( 
+  const { authState } = useContext(AuthContext);
+
+  const auth = getAuth();
+
+  const userSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("Signed out successfully");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
     <nav>
-                   <Link to="/">    <h1> Toxiplantes</h1></Link>
-        <ul>        
-            <li><AuthDetails/></li>
-            <li>
-                <Link to="/account">Compte</Link>
-            </li>
-        </ul>
+      <Link to="/">
+        {" "}
+        <h1> Toxiplantes</h1>
+      </Link>
+      <ul>
+        <li>
+          {authState ? (
+            <div>
+              <p>Bienvenue {authState.email}</p>{" "}
+              <button onClick={userSignOut}>Se déconnecter</button>{" "}
+            </div>
+          ) : (
+            <Link to="/account">Se connecter / Créer un compte</Link>
+          )}
+        </li>
+      </ul>
     </nav>
-     );
-}
- 
+  );
+};
+
 export default Navbar;
